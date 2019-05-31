df55e8a423edbb9976b3e1414b4010921db1a7e0
Gennady Evtodiev <gvevtodiev@gmail.com>
2019-06-01 02:11:25 +0300

    final 2

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$user = 'id306413_root';
+$user = 'id9793468_root';
 $password = 'root2boot';
-$db = 'id306413_lumberyard2';
+$db = 'id9793468_lumberyard2';
 $host = 'localhost';
 $port = 3306;
 ?>
